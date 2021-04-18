44acfdf9ff80ff6755d424db1997983832f4891e
flavio.coutinho <flavio.coutinho@csiway.com.br>
2021-04-18 18:22:17 -0300

    -init

---
+++ picpay-teste-backend/src/main/java/com/picpay/testebackend/PicpayTesteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.testebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayTesteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayTesteBackendApplication.class, args);
+	}
+
+}
---
+++ picpay-teste-backend/src/main/java/com/picpay/testebackend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.picpay.testebackend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PicpayTesteBackendApplication.class);
+	}
+
+}
